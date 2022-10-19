041fc98374077134e7a8a49d8235cc032645b6fd
Ge0h <george.walker.wme@gmail.com>
2022-10-19 10:25:25 +0100

    search title fetch function implemented with render results to DOM

--- assets/js/watchmode-api.js
+++ assets/js/watchmode-api.js
@@ -2,38 +2,31 @@ const apiKey = 'MB4gzmhVNVA8n71On3jvaaUYRHlKvZHTZr41yw4A'
 
 //search `https://api.watchmode.com/v1/search/?apiKey=${apiKey}&search_field=name&search_value=${searchValue}`
 //source `https://api.watchmode.com/v1/title/${title_id}/sources/?apiKey=${apiKey}`
+// autocomplete search `https://api.watchmode.com/v1/autocomplete-search/?apiKey=${apiKey}&search_value=${searchValue}&search_type=1`
 
-// const searchButton = document.querySelector('.search');
-// searchButton.addEventListener('click', searchTitle);
+const searchForm = document.querySelector('#searchForm');
+searchForm.addEventListener('submit', searchTitle);
 
-// let titleResults
+let titleResults
 const titles = document.querySelector('.posts');
 
-// function searchTitle() {
-//   titles.textContent = '';
-//   let searchTitle = document.querySelector('input').value.replace(' ', '%20');
-//   console.log(searchTitle)
-//   fetch(`https://api.watchmode.com/v1/search/?apiKey=${apiKey}&search_field=name&search_value=${searchTitle}`)
-//   .then(res => res.json())
-//   .then(data => {
-//     if(data.title_results.length === 0) {
-//       const h2 = document.createElement('h2');
-//       h2.textContent = 'No results found, please try another search term.';
-//       titles.append(h2);
-//     }
-//     console.log(data)
-//     titleResults = data.title_results
-//     data.title_results.forEach(title => {
-//       const h2 = document.createElement('h2');
-//       h2.classList.add(title.name.split(' ').join(''))
-//       h2.style.cursor = 'pointer';
-//       h2.textContent = title.name;
-//       titles.append(h2);
-//       h2.addEventListener('click', getStreamingServices);
-//     })
-//   })
-//   .catch(err => console.log(`err: ${err}`))
-// }
+function searchTitle(event) {
+  event.preventDefault();
+  titles.textContent = '';
+  let searchTitle = document.querySelector('#title').value.replace(' ', '%20');
+  fetch(`https://api.watchmode.com/v1/autocomplete-search/?apiKey=${apiKey}&search_value=${searchTitle}&search_type=1`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.results)
+    if(data.results.length === 0) {
+      const h2 = document.createElement('h2');
+      h2.textContent = 'No results found, please try another search term.';
+      titles.append(h2);
+    }
+    renderSearch(data.results);
+  })
+  .catch(err => console.log(`err: ${err}`))
+}
 
 
 // function getStreamingServices(e) {
@@ -60,23 +53,25 @@ const titles = document.querySelector('.posts');
 //   .catch(err => console.log(`err: ${err}`))
 // }
 
-const article = document.createElement('article');
-const header = document.createElement('header')
-const titleHeader = document.createElement('h2');
-const headerLink = document.createElement('a');
-headerLink.textContent = 'Sed magna';
-headerLink.href = '#';
-const imageLink = document.createElement('a');
-imageLink.classList.add('image', 'fit');
-imageLink.href = '#';
-const titleImage = document.createElement('img');
-titleImage.src = 'images/pic02.jpg';
-const titleDescrip = document.createElement('p');
-titleDescrip.textContent = 'Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.';
-titles.append(article)
-article.append(header);
-header.append(titleHeader);
-titleHeader.append(headerLink);
-article.append(imageLink);
-imageLink.append(titleImage);
-article.append(titleDescrip)
+
+function renderSearch(titleResults) {
+  titleResults.forEach(title => {
+      const article = document.createElement('article');
+      const header = document.createElement('header');
+      const titleHeader = document.createElement('h2');
+      const headerLink = document.createElement('a');
+      headerLink.textContent = title.name;
+      headerLink.href = '#';
+      const imageLink = document.createElement('a');
+      imageLink.classList.add('image', 'fit');
+      imageLink.href = '#';
+      const titleImage = document.createElement('img');
+      titleImage.src = title.image_url ? title.image_url : '';
+      titles.append(article);
+      article.append(header);
+      header.append(titleHeader);
+      titleHeader.append(headerLink);
+      article.append(imageLink);
+      imageLink.append(titleImage);
+  })
+}
